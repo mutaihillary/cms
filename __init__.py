8ce044b48226b8747220882e75dd170d8095f232
mutaihillary <mutaihillary@yahoo.com>
2016-10-04 20:05:27 +0300

    update

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from content_management import Content
+
+TOPIC_DICT = Content()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return render_template("main.html")
+
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run()
