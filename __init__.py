6e47928ec8c241b57bcc37cdf26bb07626ca0135
mutaihillary <mutaihillary@yahoo.com>
2016-10-05 10:36:25 +0300

    update

--- __init__.py
+++ __init__.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, url_for, redirect
 from content_management import Content
 
 TOPIC_DICT = Content()
 
 app = Flask(__name__)
+
+
+@app.route('/login/', methods=["GET", "POST"])
+def login_page():
+    error = ''
+    try:
+
+        if request.method == "POST":
+
+            attempted_username = request.form['username']
+            attempted_password = request.form['password']
+
+            if attempted_username == "admin" and attempted_password == "password":
+                return redirect(url_for('dashboard'))
+
+            else:
+                error = "Invalid credentials. Try Again."
+
+        return render_template("login.html", error=error)
+
+    except Exception as e:
+        # flash(e)
+        return render_template("login.html", error=error)
 
 
 @app.route('/')
@@ -13,7 +36,14 @@ def homepage():
 
 @app.route('/dashboard/')
 def dashboard():
-    return render_template("dashboard.html")
+    #flash("flash test!!!!")
+    return render_template("dashboard.html", TOPIC_DICT = TOPIC_DICT)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
 
 if __name__ == "__main__":
     app.run()
--- content_management.py
+++ content_management.py
@@ -1,7 +1,9 @@
 def Content():
-    TOPIC_DICT = {"Basics":[["Introduction to Python","/introduction-to-python-programming/"],
+    TOPIC_DICT = {"Home":[["Introduction to Python","/introduction-to-python-programming/"],
                             ["Print functions and Strings","/python-tutorial-print-function-strings/"],
                             ["Math basics with Python 3","/math-basics-python-3-beginner-tutorial/"]],
-                  "Web Dev":[]}
+                  "Profile":[["Kenya tourism board","/why you have to visit kenya in may and october/"],
+                             ["ugandan cranes","/The number twenty team in africa/"],
+                             ["team keanya","/tembea kenaya/"]]}
 
     return TOPIC_DICT
